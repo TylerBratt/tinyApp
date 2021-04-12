07927884bea5ca7e0aeae9dbedd6810e4e96eb59
Tyler Bratt <tyler~bratt@hotmail.com>
2021-04-12 19:10:19 -0400

    updated POST functionality

--- expressServer.js
+++ expressServer.js
@@ -1,39 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8080;
-const bodyParser = require('body-parser');
-const generateRandomString = () =>{
-  const charecters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const bodyParser = require("body-parser");
+const generateRandomString = () => {
+  const charecters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   const stringLength = 6;
-  let randomString = '';
+  let randomString = "";
   for (let i = 0; i < stringLength; i++) {
     let randomNum = Math.floor(Math.random() * charecters.length);
     randomString += charecters.substring(randomNum, randomNum + 1);
   }
   return randomString;
 };
-console.log(generateRandomString());
-
-app.set('view engine', 'ejs');
+const short = generateRandomString();
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
-app.get('/urls.json', (req, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/urls', (req, res)=> {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render('urlsIndex', templateVars);
+  res.render("urlsIndex", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -41,20 +40,28 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+// this line generates a string and sets it as the key
+// and makes it equal to the user entered form input
+  urlDatabase[short] = req.body.longURL;
+  res.redirect(`/urls/:${short}`);
+  console.log('LOOKEE HERE:', short);
 });
 
-app.get('/urls/:shortURL', (req, res)=> {
+app.get("/urls/:shortURL", (req, res) => {
   // :shortURL is the vaule that we enter into the browser that leads to a key in the database.
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urlsShow', templateVars);
+  res.render("urlsShow", templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body><h1>Hello World!</h1></body><!html>\n');
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
-app.listen(PORT,()=>{
+app.get("/hello", (req, res) => {
+  res.send(`<html><body><h1>Hello World!</h1></body><!html>\n`);
+});
+
+app.listen(PORT, () => {
   console.log(`Example app listening on ${PORT}!`);
 });
