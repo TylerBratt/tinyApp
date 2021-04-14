397aa5d25c392c8e5521f1d17d441863cb04b73d
Tyler Bratt <tyler~bratt@hotmail.com>
2021-04-14 14:28:07 -0400

    registration added

--- expressServer.js
+++ expressServer.js
@@ -1,41 +1,46 @@
+// REQUIRED
 const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+
+// SET and USEs
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// GENERATE THE 6 DIGIT SHORL URL
 const generateRandomString = () => {
   const newKey = Math.random().toString(36).substring(2,8);
   return newKey;
 };
 
-
+// STARTING DATABASE
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+// USERS DATABASE
+const users = {
+  userID : {
+    id: "JoeyJoeJoeJrShabadu",
+    email: 'BarneysFriend@Simpsons.com',
+    password: 'dumbestName'
+  },
+  user2ID : {
+    id: 'MontyBurns',
+    email: 'NukeSpringfield@Simpsons.com',
+    password: 'Bobo'
+  }
+};
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-  console.log('Cookies: ', req.cookies);
-  console.log('Signed Cookies: ', req.signedCookies);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
+// URLS PAGE
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase,
     username: req.cookies['username'] };
   res.render("urlsIndex", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urlsNew");
 });
 
 app.post("/urls", (req, res) => {
@@ -46,6 +51,43 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newKey}`);
 });
 
+// LOGIN - LOGOUT - REGISTER
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post('/logout', (req,res)=> {
+  res.clearCookie();
+  res.redirect('/urls');
+});
+
+app.get('/register', (req, res)=>{
+  const templateVars = {username: req.cookies.username};
+  res.render('register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  const userID = generateRandomString();
+  const username = req.body.username;
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+  users.userID = {
+    id: userID,
+    email: userEmail,
+    password: userPassword
+  };
+  res.cookie('username', username);
+  res.redirect('urls');
+});
+
+// NEW
+app.get("/urls/new", (req, res) => {
+  const templateVars = {username: req.cookies.username};
+  res.render("urlsNew", templateVars);
+});
+
+// DELETE
 //the path takes a : because it is looking for a variable
 // STOP FORGETTING THE '/'!!!!!!!!!
 app.post("/urls/:short/delete", (req, res) => {
@@ -55,13 +97,14 @@ app.post("/urls/:short/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+// INDIVIDUAL SHORT PAGE URL
 app.get("/urls/:shortURL", (req, res) => {
   // :shortURL is the vaule that we enter into the browser that leads to a key in the database.
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urlsShow", templateVars);
 });
 
-app.post("/urls/:shortURL", (req,res) => {
+app.post("/urls/:shortURL/edit", (req,res) => {
   // const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
@@ -73,15 +116,14 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
-app.post('/logout', (req,res)=> {
-  res.clearCookie();
-  res.redirect('/urls');
-})
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.get("/hello", (req, res) => {
   res.send(`<html><body><h1>Hello World!</h1></body><!html>\n`);
